dc0fe615d9fa9d73a532dfa310c5e0c4cbf8a560
DESKTOP-RCKG1AN\Vinko <vinko.kovac33@gmail.com>
2022-06-11 13:21:21 +0200

    registration and get all users working

--- WebApp/Pages/Register.cshtml.cs
+++ WebApp/Pages/Register.cshtml.cs
@@ -23,10 +23,6 @@ namespace WebApp.Pages
 
         public async Task<IActionResult> OnPost()
         {
-
-            Console.WriteLine();
-            Console.WriteLine("usao");
-            Console.WriteLine();
             if (Input.Button != "register")
             {
                 return RedirectToAction("Index");
@@ -35,54 +31,48 @@ namespace WebApp.Pages
             {
                 using (var client = new HttpClient())
                 {
-                    var token = await HttpContext.GetTokenAsync("access-token");
                     var data = new Dictionary<string, object>();
                     data.Add("username", Input.Username);
                     data.Add("firstName", Input.FirstName);
                     data.Add("lastName", Input.LastName);
                     data.Add("email", Input.Email);
                     data.Add("enabled", "true");
-                    data.Add("emailVerified", "");
 
                     var credentials = new Dictionary<string, object>();
                     credentials.Add("type", "password");
                     credentials.Add("value", Input.Password);
                     credentials.Add("temporary", "false");
                     var credArray = new List<Dictionary<string, object>>();
-                    var jsonArray = JsonConvert.SerializeObject(credArray);
-                    data.Add("credentials", jsonArray);
+                    credArray.Add(credentials);
+                    //var jsonArray = JsonConvert.SerializeObject(credArray);
+                    data.Add("credentials", credArray);
 
                     var json = JsonConvert.SerializeObject(data);
 
                     var rep = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
 
+                    Debug.WriteLine(json.ToString());
+
                     client.DefaultRequestHeaders.Add("accept", "application/json");
 
-                    /*var cookies = HttpContext.Request.Cookies;
-                    var cookie = string.Empty;
-                    foreach (var key in cookies.Keys)
-                    {
-                        cookie += key + "=" + cookies[key] + ";";
-                    }
-                    client.DefaultRequestHeaders.Add("cookie", cookie);*/
+                    var tok = await HttpContext.GetTokenAsync("access_token");
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", tok);
 
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-
-                    var response = await client.PostAsync("http://localhost:8080/realms/myrealm/users", rep);
+                    var response = await client.PostAsync("http://localhost:8080/auth/admin/realms/master/users", rep);
 
                     if (response.StatusCode != HttpStatusCode.Created)
                     {
                         Debug.WriteLine(response.StatusCode.ToString());
                         Debug.WriteLine(response.ToString());
-                        return RedirectToAction("Register");
+                        return Redirect("/Register");
                     }
-                    return RedirectToAction("Index");
+                    return Redirect("/Index");
                    
                 }
             } else
             {
                 throw new Exception("Model not valid.");
-                return RedirectToAction("Register");
+                return Redirect("/Register");
             }
         }
 
